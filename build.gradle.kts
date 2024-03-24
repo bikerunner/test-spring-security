268d866e8f62fc72a87e2c9f4c0c7a0cffe32b9d
bikerunner <18438656+bikerunner@users.noreply.github.com>
2024-03-24 23:10:37 +0100

    feat: Add initial project

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	war
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.bikerunner"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
