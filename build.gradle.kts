daeafe59b59bd1cf38ae5c0b08437e711f463790
Klaus Mueller <38108147+MuellerKlaus@users.noreply.github.com>
2024-03-27 08:34:19 +0100

    feat: Add user and permit all requests

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+//	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
