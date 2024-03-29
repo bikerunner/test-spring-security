9e25316346a368c0cb9b9ee2f5c03143609ab13d
Klaus Mueller <38108147+MuellerKlaus@users.noreply.github.com>
2024-03-29 23:16:46 +0100

    refactor: Move configuration to separate package

--- src/main/java/com/bikerunner/springsecuritytest/config/ProjectConfig.java
+++ src/main/java/com/bikerunner/springsecuritytest/config/ProjectConfig.java
@@ -1,11 +1,10 @@
-package com.bikerunner.springsecuritytest;
+package com.bikerunner.springsecuritytest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
