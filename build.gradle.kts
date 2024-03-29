838e176875b699420b78386854693ff4b7ff4ee7
Klaus Mueller <38108147+MuellerKlaus@users.noreply.github.com>
2024-03-29 23:46:21 +0100

    feat: Add user details entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+
+
 }
 
 tasks.withType<Test> {
